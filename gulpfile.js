7714c0fee90281330a89c52094bcb95aef347a76
Anthony Gilardi <acgilardi@gmail.com>
2016-01-02 23:17:42 -0500

    update for test

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
 //import * as gulp from 'gulp';
 //import {runSequence, task} from './tools/utils';
 
-var gulp = require('gulp'),
-    spawn = require('child_process').spawn,
-    node;
+var gulp = require('gulp');
 
-gulp.task('serve', function() {
-    if (node) {
-        node.kill();
-    }
-    node = spawn('node', ['server.js'], {stdio: 'inherit'})
-    node.on('close', function (code) {
-        if (code === 8) {
-            gulp.log('Error detected, waiting for changes...');
-        }
-    });
-});
+//gulp.task('serve', function() {
+//    if (node) {
+//        node.kill();
+//    }
+//    node = spawn('node', ['server.js'], {stdio: 'inherit'})
+//    node.on('close', function (code) {
+//        if (code === 8) {
+//            gulp.log('Error detected, waiting for changes...');
+//        }
+//    });
+//});
 
